a129e666b1eeca27a2fa4a0f1cd0b7210bdecc7f
selinyoon <30333564+selinyoon@users.noreply.github.com>
2020-05-30 19:51:09 +0900

    Create week-3-homework.py
    3주차과제

---
+++ week-3/week-3-homework.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
+
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+#tr:nth-child(2) > td.number
+#tr:nth-child(3) > td.info > a.title.ellipsis
+#tr:nth-child(4) > td.info > a.artist.ellipsis
+
+for song in songs:
+    a_tag = song.select_one('td > a.title.ellipsis')
+    if a_tag is not None:
+        number = song.select_one('td.number').text[0].strip()
+        title = a_tag.text.strip()
+        people = song.select_one('td.info > a.artist.ellipsis').text.strip()
+        print (number, title, people)
